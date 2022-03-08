e3414f0da0b729dae091521d4eebc499a3d847ab
r0han99 <r409067@gmail.com>
2022-03-09 02:37:10 +0530

    Class Data Distrbuted * Data is now segregated into classes of relevance * Finetuned the correlational part of the code to improve the sample collection * Changed the code structure for displaying fundus * Pending works     * Contrast plots     * Data Analysis     * Tensorflow repository structure

--- Notebooks/load_classes.py
+++ Notebooks/load_classes.py
@@ -1,37 +1,39 @@
 import re
 
-def fetch_classdict():
-    with open('class_names.txt','r') as f:
-        content = list(f.readlines())
-    combined = ' '.join(content)
+def fetch_classdict(mode='all'):
+    if mode == 'all':
+        with open('class_names.txt','r') as f:
+            content = list(f.readlines())
+        combined = ' '.join(content)
 
-    # fetching chars
-    pattern = r'\(([A-Z]{1})\)'
-    finds = re.findall(pattern, combined)
+        # fetching chars
+        pattern = r'\(([A-Z]{1})\)'
+        finds = re.findall(pattern, combined)
 
-    # packaging
-    classes = {}
-    for item, token in zip(content, finds):
-        item = item.split(' ')[0]
-        classes[token] = item
-        
+        # packaging
+        classes = {}
+        for item, token in zip(content, finds):
+            item = item.split(' ')[0]
+            classes[token] = item
+
+        return classes
+    else:
+        with open('coi.txt','r') as f:
+            content = list(f.readlines())
+        combined = ' '.join(content)
+
+        # fetching chars
+        pattern = r'\(([A-Z]{1})\)'
+        finds = re.findall(pattern, combined)
+
+        # packaging
+        classes = {}
+        for item, token in zip(content, finds):
+            item = item.split(' ')[0]
+            classes[token] = item
+
     return classes
-
-def fetch_coi():
-
-    with open('coi.txt','r') as f:
-        content = list(f.readlines())
-    combined = ' '.join(content)
-
-    # fetching chars
-    pattern = r'\(([A-Z]{1})\)'
-    finds = re.findall(pattern, combined)
-
-    # packaging
-    classes = {}
-    for item, token in zip(content, finds):
-        item = item.split(' ')[0]
-        classes[token] = item
         
-    return classes
+
+
 
